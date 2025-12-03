8b012c43bacef975ae1697ff21cc6393e363e534
Freaking-Bob <lyonico645@gmail.com>
2025-12-02 23:02:17 -0600

    FastRAG_ollama

--- 395-scripts/fastgraphrag/gpt-oss-20b/run_medical.sh
+++ 395-scripts/fastgraphrag/gpt-oss-20b/run_medical.sh
@@ -1,8 +1,8 @@
-python 395-scripts/fastgraphrag/qwen3_8b/fast-graphrag_ollama.py \
+python 395-scripts/fastgraphrag/gpt-oss-20b/fast-graphrag_ollama.py \
     --subset medical \
-    --model_name qwen3:8b \
+    --model_name gpt-oss-20b \
     --sample 3 \
     --embed_model_path BAAI/bge-large-en-v1.5 \
     --llm_base_url http://127.0.0.1:11434/v1 \
     --llm_api_key ollama \
-    --base_dir ./Examples/graphrag_qwen3_8b_medical
+    --base_dir ./Examples/graphrag_gpt-oss-20b_medical
---
+++ 395-scripts/fastgraphrag/gpt-oss-20b/run_novel.sh
@@ -0,0 +1,7 @@
+python 395-scripts/fastgraphrag/gpt-oss-20b/fast-graphrag_ollama.py \
+    --subset novel \
+    --model_name gpt-oss-20b \
+    --embed_model_path BAAI/bge-large-en-v1.5 \
+    --llm_base_url http://127.0.0.1:11434/v1 \
+    --llm_api_key ollama \
+    --base_dir ./Examples/graphrag_gpt-oss-20b_medical
